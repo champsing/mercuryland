3c8a03f2f624a0489f2f2f4a5cf091542fe82eeb
LogicFan <fanyongda2012@gmail.com>
2024-05-23 18:21:20 -0400

    convert to TS

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
-<script setup>
+<script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <HelloWorld />
+    <HelloWorld />
 </template>
 
 <style scoped></style>
