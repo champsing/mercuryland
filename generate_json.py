c759b252af1819ceb9f774c208a24bd036593cc3
champsing <88385811+champsing@users.noreply.github.com>
2024-06-05 15:56:04 +0800

    update vod.json and vod.json template

--- generate_json.py
+++ generate_json.py
@@ -2,14 +2,14 @@ import json
 
 res = []
 
-for i in range(100, 201):
+for i in range(117, 201):
     res.append(
     {
         "id": i,
         "date": "2024",
         "link": "",
         "title": "【惡靈】",
-        "tags": ["MC原味生存", "MC地下城", "MC盖地图", "MC空岛生存"],
+        "tags": [],
         "duration": ""
     },
 )
