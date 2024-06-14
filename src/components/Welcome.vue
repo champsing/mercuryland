1f7eaddd0ee8252e09224b62c77bb5fc5301c4cb
Logic <38597904+LogicFan@users.noreply.github.com>
2024-06-14 03:37:56 -0400

    fixup

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,5 +2,5 @@
 </script>
 
 <template>
-    <img src="/image/welcome.png" class="z-0 w-fit -mt-3" alt="Welcome"/>
+    <img src="../assets/image/welcome.png" class="z-0 w-fit -mt-3" alt="Welcome"/>
 </template>
