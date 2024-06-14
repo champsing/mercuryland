fd592bfac40efc52faab19bfa26cdf5f2fa7f8d1
champsing <88385811+champsing@users.noreply.github.com>
2024-06-14 22:33:05 +0800

    Update generate_json.py
    remove comment

--- generate_json.py
+++ generate_json.py
@@ -15,7 +15,6 @@ for i in range(117, 201):
 )
 
 with open('./src/assets/numbered_vod.json', 'w', encoding='utf-8') as f:
-    # 移動檔案指針到開頭，並寫入更新後的行到文本檔案中
     json.dump(res, f, ensure_ascii=False)
 
 print("已將 JSON 檔案中的每一行設置為文本檔案對應行的值。")
