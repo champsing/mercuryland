22a9820ada829c39be3f9f1c3e8b2982db2d57a9
Logic <38597904+LogicFan@users.noreply.github.com>
2024-06-15 04:31:25 -0400

    fix object-fit

--- src/components/welcome/Slide1.vue
+++ src/components/welcome/Slide1.vue
@@ -5,8 +5,9 @@
 <template>
     <div class="h-screen overflow-hidden">
         <img
+            object-fit="cover"
             src="@assets/images/welcome.png"
-            class="z-10"
+            class="z-10 w-full h-full object-cover"
             alt="Welcome"
         />
     </div>
--- src/components/welcome/Slide2.vue
+++ src/components/welcome/Slide2.vue
@@ -6,7 +6,7 @@
     <div class="h-screen overflow-hidden">
         <img
             src="@assets/images/S1_close_1.png"
-            class="z-10"
+            class="z-10 w-full h-full object-cover"
             alt="Welcome"
         />
     </div>
