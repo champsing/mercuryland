4c7ed08f4d072e31a5401ab50f13d96c212e4599
Logic <38597904+LogicFan@users.noreply.github.com>
2024-06-16 10:11:01 -0400

    add slide 3

--- src/components/welcome/Slide3.vue
+++ src/components/welcome/Slide3.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
-// import Placeholder from "./welcome/Placeholder.vue"
+import { NGrid, NGi, NDivider } from "naive-ui";
+import NextPageButton from "./NextPageButton.vue";
 </script>
 
 <template>
-    <div class="h-screen overflow-hidden">
-        <img
-            src="@assets/images/S1_close_1.png"
-            class="z-10 w-full h-full object-cover"
-            alt="Welcome"
-        />
+    <div class="h-screen overflow-hidden bg-teal-800 relative">
+        <NextPageButton :page="3" />
+        <n-grid
+            x-gap="64"
+            :cols="2"
+            item-responsive
+            class="absolute x-center w-10/12 h-full"
+        >
+            <n-gi span="0 800:1" class="relative">
+                <img
+                    src="@assets/images/welcome/statue_of_liberty.png"
+                    class="absolute y-center x-center h-5/6 object-contain"
+                    alt="Welcome"
+                />
+            </n-gi>
+            <n-gi span="2 800:1" class="relative">
+                <div class="absolute y-center x-center">
+                    <div class="text-cyan-600 font-bold text-6xl">
+                        發揮您的創意，建造您夢想中的建築。
+                    </div>
+                    <n-divider />
+                    <div class="text-cyan-600 font-bold text-3xl">
+                        絕無思想審查制度。您可以在我們的伺服器復刻世界上的有名建築，或單單只是在這個世界蓋出專屬於您的根據地。
+                    </div>
+                </div>
+            </n-gi>
+        </n-grid>
     </div>
 </template>
