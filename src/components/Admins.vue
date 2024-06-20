2776025f6bfb552251558068dbf3dbe40b615b5a
Logic <38597904+LogicFan@users.noreply.github.com>
2024-06-20 13:10:41 -0400

    add contact

--- src/components/Admins.vue
+++ src/components/Admins.vue
@@ -1,36 +1,48 @@
 <script setup lang="ts">
+import { NGrid, NGi } from "naive-ui";
+import ContactCard from "./contact/ContactCard.vue";
 </script>
 
 <template>
-    <div class="admin_intro">
-        伺服器管理員
-    </div>
-    <img src="@assets/images/admin_avatars/avatar_oreki20.png" />
-    <div class="oreki">
-        惡靈 Oreki
-    </div>
-    <div class="oreki_intro">
-        伺服主（CEO）、伺服器總管、伺服器白名單管理、水星伺服器管理委員會主委、爭議協調與排解委員
-    </div>
-    <div class="youtube_button">
-        惡靈YouTube頻道按鈕
-    </div>
-    <img src="@assets/images/admin_avatars/avatar_champsing.png" />
-    <div class="champsing">
-        香榭 Champsing
-    </div>
-    <div class="champsing_intro">
-        網站設計、伺服器管理、伺服器資料庫維護、伺服器營運總監、創意總監、法務總監、水星伺服器管理委員會委員、爭議協調與排解委員
-    </div>
-    <div class="logic">
-        邏輯 Logic
-    </div>
-    <img src="@assets/images/admin_avatars/avatar_logic.png" />
-    <div class="logic_intro">
-        網站設計
-    </div>
-
-    <n-grid x-gap="12" :cols="4" class="w-11/12">
-        
+    <n-grid x-gap="12" y-gap="12" cols="3" class="w-11/12" item-responsive>
+        <n-gi span="3 800:1">
+            <ContactCard
+                name1="惡靈"
+                name2="Oreki"
+                title="水星神, 伺服器總管, 爭議協調與排解委員"
+                discord="oreki20"
+            >
+                <img
+                    src="@assets/images/admin_avatars/avatar_oreki20.png"
+                    class="w-full h-full object-contain"
+                />
+            </ContactCard>
+        </n-gi>
+        <n-gi span="3 800:1">
+            <ContactCard
+                name1="香榭"
+                name2="Champsing"
+                title="伺服器維護, 網站設計, 爭議協調與排解委員"
+                discord="champsing"
+            >
+                <img
+                    src="@assets/images/admin_avatars/avatar_champsing.png"
+                    class="w-full h-full object-contain"
+                />
+            </ContactCard>
+        </n-gi>
+        <n-gi span="3 800:1">
+            <ContactCard
+                name1="邏輯"
+                name2="Logic"
+                title="摸鱼专家"
+                discord="logic_f"
+            >
+                <img
+                    src="@assets/images/admin_avatars/avatar_logic.png"
+                    class="w-full h-full object-contain"
+                />
+            </ContactCard>
+        </n-gi>
     </n-grid>
 </template>
---
+++ src/components/contact/ContactCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { NCard, NDivider } from "naive-ui";
+
+const props = defineProps<{
+    name1: string;
+    name2: string;
+    title: string;
+    discord: string;
+}>();
+</script>
+
+<template>
+    <div class="w-2/3 max-w-72 h-full m-auto">
+        <n-card :title="props.name1" class="h-full">
+            <template #header-extra>{{ props.name2 }}</template>
+            <template #footer>discord@{{ props.discord }}</template>
+            <div class="w-full aspect-square">
+                <slot></slot>
+            </div>
+            <n-divider />
+            {{ props.title }}
+        </n-card>
+    </div>
+</template>
