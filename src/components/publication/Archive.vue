1bbc1cefe61fcb154dff95be91ac55688e8651ec
champsing <88385811+champsing@users.noreply.github.com>
2024-06-23 10:53:29 +0800

    add S1, S2 to S5

--- src/components/publication/Archive.vue
+++ src/components/publication/Archive.vue
@@ -8,6 +8,28 @@ import { ccMix } from '@/composables/utils';
         <n-gi span="3 800:1">
             <iframe 
                 width="410" height="400" 
+                src="https://www.mediafire.com/file/91h9afekvfnuymi/mercury_world.rar/file"
+                title="S1"
+            >
+            </iframe>
+            <div class="text-2xl text-neutral-100 text-center mt-4 mr-10">
+                {{ ccMix("第一季") }}
+            </div>
+        </n-gi>
+        <n-gi span="3 800:1">
+            <iframe 
+                width="410" height="400" 
+                src="https://drive.google.com/file/d/12ka9h87OkzmSrRiPTPhFhs_-Q-sfcCm7/view"
+                title="S2 to S5"
+            >
+            </iframe>
+            <div class="text-2xl text-neutral-100 text-center mt-4 mr-10">
+                {{ ccMix("第二～五季") }}
+            </div>
+        </n-gi>
+        <n-gi span="3 800:1">
+            <iframe 
+                width="410" height="400" 
                 src="https://drive.google.com/file/d/11JeFAEeQHUnSpxRajxiJlKt5o7dRTMh6/view" 
                 title="S6, 7"
             >
@@ -15,10 +37,6 @@ import { ccMix } from '@/composables/utils';
             <div class="text-2xl text-neutral-100 text-center mt-4 mr-10">
                 {{ ccMix("第六、七季") }}
             </div>
-        </n-gi>
-        <n-gi span="3 800:1">
-        </n-gi>
-        <n-gi span="3 800:1">
         </n-gi>
     </n-grid> 
 </template>
