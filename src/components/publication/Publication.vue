7c5d58a00bcd33f7d96054049782c63fe2b4ee38
champsing <88385811+champsing@users.noreply.github.com>
2024-06-24 00:22:07 +0800

    第五版水星法

--- src/components/publication/Publication.vue
+++ src/components/publication/Publication.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { NDivider } from 'naive-ui';
+import { NDivider, NGrid, NGi } from 'naive-ui';
 import { ccMix } from '@/composables/utils';
 import Law from "./Law.vue";
 import Archive from './Archive.vue';
@@ -9,6 +9,25 @@ import Archive from './Archive.vue';
     <div class="text-2xl text-neutral-100 text-center mt-4">
         {{ ccMix("在這裡可以找到所有規則、伺服器名簿與檔案。") }}
     </div>
+    <n-divider />
+    <n-grid x-gap="12" y-gap="12" cols="3" class="w-11/12" item-responsive>
+        <n-gi span="3 800:1"></n-gi>
+        <n-gi span="3 800:1">
+            <iframe 
+                width="410" height="400" 
+                src="https://drive.google.com/file/d/1Lti-30AKf4cz9feAp6sfxyT0p82awT2F/preview?usp=drive_link" 
+                title="水星法"
+            >
+            </iframe>
+            <div class="text-2xl text-neutral-100 text-center mt-4 mr-10">
+                {{ ccMix("《水星法》公告版本") }}
+            </div>
+            <div class="text-1xl text-neutral-100 text-center mt-4 mr-8">
+                {{ ccMix("公告一週後實施。") }}
+            </div>
+        </n-gi>
+        <n-gi span="3 800:1"></n-gi>
+    </n-grid>
     <n-divider />
         <div class="text-4xl text-neutral-100 text-left mt-4 mb-6">
             {{ ccMix("法規與資料") }}
