4c53075494972de2fcfd45614bd808c8549d5afb
Logic <38597904+LogicFan@users.noreply.github.com>
2024-06-24 16:23:10 -0400

    fixup

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 import Welcome from "@components/welcome/Welcome.vue";
 import Vod from "@components/vod/Vod.vue";
 import GameMap from "@components/GameMap.vue";
@@ -18,7 +18,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes,
 });
 
