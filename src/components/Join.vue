1dbdb856e7ee3781186986e01478ad179beba709
Logic <38597904+LogicFan@users.noreply.github.com>
2024-06-28 13:04:50 -0400

    fixup

--- src/components/Join.vue
+++ src/components/Join.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
-import { NButton, NButtonGroup, NCard, NGrid, NGi, NIcon, NStep, NSteps } from "naive-ui";
+import {
+    NButton,
+    NButtonGroup,
+    NCard,
+    NGrid,
+    NGi,
+    NIcon,
+    NStep,
+    NSteps,
+} from "naive-ui";
 import { ccMix, copyToClipboard, openLink } from "@composables/utils";
 import { ref } from "vue";
-import { MdArrowRoundBack, MdArrowRoundForward } from '@vicons/ionicons4'
+import { MdArrowRoundBack, MdArrowRoundForward } from "@vicons/ionicons4";
 //TODO: Server Online Status
 //import { IosRadioButtonOn } from "@vicons/ionicons4";
 
@@ -13,33 +22,28 @@ import { MdArrowRoundBack, MdArrowRoundForward } from '@vicons/ionicons4'
 let currentStep = ref<number | null>(1);
 //current step
 
-function clickLinkButton(link: string, toBePlus1: number) {
+function clickLinkButton(link: string) {
     openLink(link);
-    toBePlus1++;
+    currentStep.value++;
 }
 
 //Prev/Next Button
 
 function next() {
-    if (currentStep.value === null)
-        currentStep.value = 1
-    else if (currentStep.value >= 4)
-        currentStep.value = null
-    else currentStep.value++
+    if (currentStep.value === null) currentStep.value = 1;
+    else if (currentStep.value >= 4) currentStep.value = null;
+    else currentStep.value++;
 }
 
 function prev() {
-    if (currentStep.value === 0)
-        currentStep.value = null
-    else if (currentStep.value === null)
-        currentStep.value = 4
-    else currentStep.value--
+    if (currentStep.value === 0) currentStep.value = null;
+    else if (currentStep.value === null) currentStep.value = 4;
+    else currentStep.value--;
 }
-
 </script>
 
 <template>
-    <br/>
+    <br />
     <div class="text-center mb-4">
         <div class="text-6xl perspective-x-30 text-cyan-400">
             {{ ccMix("現在就立刻加入我們") }}
@@ -57,65 +61,74 @@ function prev() {
             </n-card>
         </n-gi>
         <n-gi span="3 800:1" class="text-center">
-            <n-card title="Seed" @click="copyToClipboard('-9100272987300380909')">
-                <n-button text class="!text-2xl">{{ ccMix("-9100272987300380909") }}</n-button>
+            <n-card
+                title="Seed"
+                @click="copyToClipboard('-9100272987300380909')"
+            >
+                <n-button text class="!text-2xl">{{
+                    ccMix("-9100272987300380909")
+                }}</n-button>
             </n-card>
         </n-gi>
         <n-gi span="3 800:1" class="text-center">
             <n-card title="Version">
-                <div class="!text-2xl">{{ ccMix("正版") }} Minecraft Java 1.21</div>
+                <div class="!text-2xl">
+                    {{ ccMix("正版") }} Minecraft Java 1.21
+                </div>
             </n-card>
         </n-gi>
     </n-grid>
 
     <!-- TODO: prev/next button -->
-    <n-steps :current=currentStep :horizontal="true" class="w-full mt-10">
+    <n-steps :current="currentStep" :horizontal="true" class="w-full mt-10">
         <n-step :title="ccMix('加入群組')" class="text-1xl w-1/4">
-            {{ ccMix("加入我們的Discord群組來申請白名單") }}
-            <br/>
-            <n-button @click="clickLinkButton('https://discord.gg/A2cMZRr', currentStep)" class="mt-2 mb-2">
-                {{ ccMix("加入 水星人的夢幻樂園") }}
+            {{ ccMix("加入水星人的夢幻樂園Discord群组") }}
+            <br />
+            <n-button
+                @click="clickLinkButton('https://discord.gg/A2cMZRr')"
+                class="mt-2 mb-2"
+            >
+                {{ ccMix("點擊加入群组") }}
             </n-button>
-            <br/>
+            <br />
             {{ ccMix("或使用連結") }}：https://discord.gg/A2cMZRr
         </n-step>
 
         <n-step :title="ccMix('閱讀規則')" class="text-1xl w-1/4">
-            {{ ccMix("包含《水星法》、《水星伺服器破壞舉報獎勵規則》等。") }} 
-            <br/>
-            <div class="w-full m-auto  mt-2 mb-2">  
-                <n-button size="large" @click="clickLinkButton('https://mercuryland.online/#/publication', currentStep)">
-                    {{ ccMix("點擊閱讀")}}
+            {{ ccMix("包含《水星法》、《水星伺服器破壞舉報獎勵規則》等。") }}
+            <br />
+            <div class="w-full m-auto mt-2 mb-2">
+                <n-button
+                    @click="
+                        clickLinkButton(
+                            'https://mercuryland.online/#/publication'
+                        )
+                    "
+                >
+                    {{ ccMix("點擊閱讀规则") }}
                 </n-button>
             </div>
         </n-step>
 
-        <n-step class="text-2xl w-1/4"
-        :title="ccMix('申請白名單')"
-        description="在 #申請伺服 打上Minecraft ID"
-        />
+        <n-step class="text-1xl w-1/4" :title="ccMix('申請白名單')">
+            {{ ccMix("在 #申請伺服 打上Minecraft ID") }}
+            <br />
+            <div class="w-full m-auto mt-2 mb-2">
+                <n-button
+                    @click="clickLinkButton('https://discord.gg/CXSQq4nVAH')"
+                >
+                    {{ ccMix("點擊跳转频道") }}
+                </n-button>
+            </div>
+        </n-step>
 
-        <n-step  class="text-2xl w-1/4"
-        :title="ccMix('等待通過')" line-type="dashed"
-        description="等待白名單申請通過"
+        <n-step
+            class="text-1xl w-1/4"
+            :title="ccMix('等待通過')"
+            line-type="dashed"
+            description="等待白名單申請通過"
         />
     </n-steps>
-    <n-button-group class="mt-4">
-        <n-button @click="prev">
-          <template #icon>
-            <n-icon>
-              <md-arrow-round-back />
-            </n-icon>
-          </template>
-        </n-button>
-        <n-button @click="next">
-          <template #icon>
-            <n-icon>
-              <md-arrow-round-forward />
-            </n-icon>
-          </template>
-        </n-button>
-      </n-button-group>
 </template>
 
 <style>
