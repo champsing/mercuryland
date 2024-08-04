5d0a99db310c214223315feec71d5d64330d730f
champsing <88385811+champsing@users.noreply.github.com>
2024-08-04 23:10:29 +0800

    move numbered vod json to /

--- generate_json.py
+++ generate_json.py
@@ -14,7 +14,7 @@ for i in range(117, 201):
     },
 )
 
-with open('./src/assets/numbered_vod.json', 'w', encoding='utf-8') as f:
+with open('./numbered_vod.json', 'w', encoding='utf-8') as f:
     json.dump(res, f, ensure_ascii=False)
 
 print("已將 JSON 檔案中的每一行設置為文本檔案對應行的值。")
