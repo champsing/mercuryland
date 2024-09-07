5de1ece0fb5d253edd71f09c3d60d24a979c2985
champsing <88385811+champsing@users.noreply.github.com>
2024-09-07 17:08:38 +0800

    dynamic options for VaMenuList
    Add Desc and Button

--- src/components/publication/Law.vue
+++ src/components/publication/Law.vue
@@ -1,18 +1,54 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { VaSplit, VaMenuList } from "vuestic-ui";
+import { Ref, ref } from "vue";
+import { NIcon } from "naive-ui";
+import { VaButton, VaDivider, VaSplit, VaMenuList } from "vuestic-ui";
 import lawDocument from "@assets/data/law_document.json";
-
-function findDocId(doc: string) {
-    let currentDocument = lawDocument.filter((v) => v.name == doc)[0];
-    previewLink.value = currentDocument.url;
-    
-}
+import { WindowNew20Filled } from "@vicons/fluent";
+import { ccMix, openLink } from "@/composables/utils";
 </script>
 
 <script select-doc lang="ts">
-let docList = lawDocument.map((x) => x.name)
-let previewLink = ref("")
+class LawDocEntry {
+    id: number;
+    name: string;
+    description: string;
+    url: string;
+}
+
+class LawMenuEntry {
+    id: number;
+    text: string;
+    value: string;
+    group: string;
+}
+
+let currentDocument: Ref<LawDocEntry> = ref({
+    id: 0,
+    name: "《水星法》",
+    description: "水星伺服器法律支柱。",
+    url: "https://drive.google.com/file/d/17zIPRyN0BpR7TU10ARDJwkKVV-zLpsz8/preview?usp=drive_link",
+    group: "",
+});
+
+function findCurrentDoc(doc: string) {
+    let target = lawDocument.filter((v) => v.name == doc)[0];
+    currentDocument.value = target;
+}
+
+let docOptions: Ref<LawMenuEntry[]> = ref(parseOptions(lawDocument));
+
+function parseOptions(law_document: typeof lawDocument) {
+    let optionArray = [];
+    for (let i = 0; i < law_document.length; i++) {
+        optionArray[i] = {
+            id: law_document[i].id,
+            text: law_document[i].name,
+            value: law_document[i].name,
+            group: lawDocument[i].group,
+        };
+    }
+    return optionArray;
+}
 </script>
 
 <template>
@@ -20,10 +56,34 @@ let previewLink = ref("")
         <template #start>
             <!-- need to be 23% -->
             <VaMenuList
-                class="text-white text-2xl"
-                :options=docList
-                @selected="(doc) => findDocId(doc)"
+                class="text-white text-lg doc-menu-hover"
+                :options="docOptions"
+                @selected="(doc) => findCurrentDoc(doc.text)"
             />
+            <!-- group name too low, need mb-2 -->
+            <VaDivider />
+            <div class="text-zinc-300 text-center text-3xl mt-4">
+                {{ ccMix(currentDocument.name) }}
+            </div>
+            <div class="text-zinc-300 text-center text-lg mt-4">
+                {{ ccMix(currentDocument.description) }}
+            </div>
+            <div>
+                <!-- need further adjust -->
+                <VaButton
+                    round
+                    size="medium"
+                    color="#38b67d"
+                    @click="openLink(currentDocument.url)"
+                >
+                    <n-icon>
+                        <WindowNew20Filled/>
+                    </n-icon>
+                    <div class="mt-1 mr-2 text-center">
+                        {{ ccMix("在新分頁開啟") }}
+                    </div>
+                </VaButton>
+            </div>
         </template>
         <template #end>
             <iframe
@@ -31,47 +91,22 @@ let previewLink = ref("")
                 width="625"
                 height="600"
                 frameborder="0"
-                :src=previewLink.valueOf()
+                :src="currentDocument.url"
                 title="preview iframe"
             >
             </iframe>
         </template>
     </VaSplit>
-    <!-- need description text and button to openLink() -->
 </template>
 
 <style lang="scss" scoped>
-.doc-tab {
-    --va-tab-height: auto;
-    --va-tab-max-width: 20px;
-    --va-tabs-container-height: auto;
-    --va-tab-content-padding: 0.4rem 0.75rem;
-    --va-tabs-slider-width: 0.7rem;
-    --va-tabs-slider-height: 0.7rem;
+.color-picker {
+    color: #38b67d;
 }
-
-.doc-split {
-    position: relative;
-    height: 40rem;
-
-    & .preview-block {
-        position: relative;
-        height: 100%;
-        overflow: hidden;
-        user-select: none;
-    }
-
-    .split-view {
-        position: absolute;
-        top: 2;
-
-        &--start {
-            left: 0;
-        }
-
-        &--end {
-            right: 0;
-        }
-    }
+.doc-menu-hover {
+    --va-menu-item-hover-color: #e13535;
+    --va-menu-item-hover-opacity: 0.6;
+    --va-menu-padding-x: 8px;
+    --va-menu-padding-y: 10px;
 }
 </style>
