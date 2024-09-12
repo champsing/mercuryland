fe4c113de82a52ea95df494e163d7238922f391b
champsing <88385811+champsing@users.noreply.github.com>
2024-09-12 14:17:05 +0800

    adjust iframe in NewLaw

--- src/components/publication/NewLaw.vue
+++ src/components/publication/NewLaw.vue
@@ -7,17 +7,17 @@ const effectiveDate = "2024 年 7 月 1 日";
 </script>
 
 <template>
-    <n-grid x-gap="12" y-gap="12" cols="2" class="w-11/12" item-responsive>
-        <n-gi span="6 800:1">
-            <n-icon> </n-icon>
-            <div class="ml-20">
+    <n-grid x-gap="12" y-gap="10" cols="2" class="w-11/12" item-responsive>
+        <n-gi span="2 800:1">
+            <!-- <n-icon> </n-icon> -->
+            <div class="-ml-28">
                 <div class="text-4xl text-red-400 text-center mt-24">
                     {{ ccMix("《水星法》新法公告") }}
                 </div>
-                <div class="text-1xl text-red-400 text-center mt-4">
+                <div class="text-2xl text-red-400 text-center mt-4">
                     {{ ccMix("修訂之條文於公告一週後實施。") }}
                 </div>
-                <div class="text-1xl text-lime-400 text-center mt-4">
+                <div class="text-2xl text-lime-400 text-center mt-4">
                     {{ ccMix("公告日期：") + publishDate }}
                 </div>
                 <div
@@ -27,9 +27,9 @@ const effectiveDate = "2024 年 7 月 1 日";
                 </div>
             </div>
         </n-gi>
-        <n-gi span="6 800:1" class="ml-20">
+        <n-gi span="10 800:1" class="-ml-20">
             <iframe
-                width="410"
+                width="700"
                 height="400"
                 src="https://drive.google.com/file/d/1Lti-30AKf4cz9feAp6sfxyT0p82awT2F/preview?usp=drive_link"
                 title="水星法新法公告"
