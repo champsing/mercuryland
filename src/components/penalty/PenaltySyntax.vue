b9fbdd6676573b6df892ddeec1b1a54aa2d34a21
champsing <88385811+champsing@users.noreply.github.com>
2024-09-15 05:37:22 +0800

    change n-collapse to VaCard

--- src/components/penalty/PenaltySyntax.vue
+++ src/components/penalty/PenaltySyntax.vue
@@ -1,57 +1,24 @@
 <script setup lang="ts">
-import {
-    NCollapse,
-    NCollapseItem,
-    NList,
-    NListItem,
-    NThing,
-} from "naive-ui";
+import { VaCard, VaCardContent, VaCardTitle } from "vuestic-ui";
 </script>
 
 <template>
-    <n-collapse
-        arrow-placement="right"
-        style="
-            --n-title-font-size: 24px;
-            --n-title-text-color: rgb(11, 118, 225);
-        "
-    >
-        <!-- <n-collapse-item title="懲罰語法" name="punish_syntax">
-            <div style="overflow: auto">
-                <n-list bordered>
-                    <n-list-item>
-                        <n-thing style="text-align: left; font-size: 18px">
-                            &lt;日期&gt;: Unix Timestamp<br />
-                            &lt;編號&gt;: int &lt;懲罰主文&gt;: string
-                            〔詳細資料〕: additionalMetaData（完成狀態）:
-                            statusMetaData
-                        </n-thing>
-                    </n-list-item>
-                </n-list>
-            </div>
-        </n-collapse-item> -->
-        <n-collapse-item title="詳細資料" name="more_information">
-            <div>
-                <n-list bordered>
-                    <n-list-item>
-                        <n-thing style="font-size: 18px">
-                            🆙增加、🔁重抽、2️⃣備案、📝原主人修改n次、➕其他後來增加的條件
-                        </n-thing>
-                    </n-list-item>
-                </n-list>
-            </div>
-        </n-collapse-item>
-        <n-collapse-item title="完成狀態" name="punish_status">
-            <div>
-                <n-list bordered>
-                    <n-list-item>
-                        <n-thing style="font-size: 18px">
-                            ✅完成、✅已抽、🏁原主人或投票給過、⏲️
-                            ⚔️目前已完成進度
-                        </n-thing>
-                    </n-list-item>
-                </n-list>
-            </div>
-        </n-collapse-item>
-    </n-collapse>
+    <div class="flex flex-wrap gap-5">
+        <VaCard color="primary" class="ml-20">
+            <VaCardTitle style="font-size: 16px">詳細資料</VaCardTitle>
+            <VaCardContent>
+                <div class="text-base">
+                    🆙增加、🔁重抽、2️⃣備案、📝原主人修改n次、➕其他後來增加的條件
+                </div>
+            </VaCardContent>
+        </VaCard>
+        <VaCard>
+            <VaCardTitle style="font-size: 16px">完成狀態</VaCardTitle>
+            <VaCardContent>
+                <div class="text-base">
+                    ✅完成、✅已抽、🏁原主人或投票給過、⏲️⚔️目前已完成進度
+                </div>
+            </VaCardContent>
+        </VaCard>
+    </div>
 </template>
