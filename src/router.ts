f840ed2a24c816b5f25567df2069bc0449b9012c
champsing <88385811+champsing@users.noreply.github.com>
2024-09-22 19:16:18 +0800

    file routing

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,19 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 import Vod from "@components/vod/Vod.vue";
 import Penalty from "@components/penalty/Penalty.vue";
-// import Welcome from "@components/welcome/Welcome.vue";
-// import Join from "@/components/join/Join.vue";
+import Welcome from "@components/welcome/Welcome.vue";
+import Join from "@/components/join/Join.vue";
 import Publication from "@components/publication/Publication.vue";
 // import GameMap from "@components/GameMap.vue";
 import Contact from "@components/contact/Contact.vue";
 import ToS from "./components/law/ToS.vue";
 
 const routes = [
-    // { path: "/", component: Welcome },
-    // { path: "/join", component: Join },
+    { path: "/", component: Welcome },
+    { path: "/join", component: Join },
     { path: "/publication", component: Publication },
     // { path: "/map", component: GameMap },
-    { path: "/", component: Vod },
+    { path: "/vod", component: Vod },
     { path: "/penalty", component: Penalty },
     { path: "/contact", component: Contact },
     { path: "/tos", component: ToS}
