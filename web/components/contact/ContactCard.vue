efeff60b02c0dcf7d943c736a6d6d6f47d1fd5d1
champsing <88385811+champsing@users.noreply.github.com>
2024-10-19 13:16:33 +0800

    deprecate naive-ui in ContactCard

--- web/components/contact/ContactCard.vue
+++ web/components/contact/ContactCard.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import { copyToClipboard } from "@/composables/utils";
-import { NButton, NCard, NTag } from "naive-ui";
-import { VaDivider } from "vuestic-ui";
+import {
+    VaButton,
+    VaCard,
+    VaCardTitle,
+    VaCardContent,
+    VaChip,
+    VaDivider,
+} from "vuestic-ui";
 
 const props = defineProps<{
     name1: string;
@@ -13,31 +19,57 @@ const props = defineProps<{
 
 <template>
     <div class="w-2/3 max-w-72 h-full m-auto">
-        <n-card :title="props.name1" class="h-full">
-            <template #header-extra>{{ props.name2 }}</template>
-            
-            <template #footer>
-                <div class="text-center">
-                    Discord: 
-                    <br/>
-                    <n-button
-                    :text="true"
-                    :focusable="false"
-                    @click="copyToClipboard(props.discord.toString())">
-                        <div class="font-bold">
-                            @{{ props.discord }}
-                        </div>    
-                    </n-button>
+        <VaCard class="h-full w-full ContactCard">
+            <VaCardTitle>
+                <div class="flex w-full justify-between">
+                    <div class="text-2xl">
+                        {{ props.name1 }}
+                    </div>
+                    <div class="text-base mt-1">
+                        {{ props.name2 }}
+                    </div>
                 </div>
-            </template>
+            </VaCardTitle>
 
-            <div class="w-full aspect-square">
-                <slot></slot>
-            </div>
-            <VaDivider />
-            <template v-for="tag in props.tags">
-                <n-tag class="m-1"> {{ tag }} </n-tag>
-            </template>
-        </n-card>
+            <VaCardContent>
+                <div class="w-full aspect-square">
+                    <slot></slot>
+                </div>
+                <VaDivider />
+                <div class="flex flex-col h-full !justify-between">
+                    <div>
+                        <VaChip
+                            class="m-1 text-base"
+                            color="#d9d9d9"
+                            outline
+                            readonly
+                            v-for="tag in props.tags"
+                        >
+                            {{ tag }}
+                        </VaChip>
+                    </div>
+
+                    <div class="text-center mt-6">
+                        Discord:
+                        <br />
+                        <VaButton
+                            preset="plain"
+                            color="#d9d9d9"
+                            hover-mask-color="#5bc6a1"
+                            hover-opacity="1"
+                            @click="copyToClipboard(props.discord.toString())"
+                        >
+                            <div class="font-bold">@{{ props.discord }}</div>
+                        </VaButton>
+                    </div>
+                </div>
+            </VaCardContent>
+        </VaCard>
     </div>
 </template>
+
+<style>
+.ContactCard {
+    --va-card-overflow: hidden;
+}
+</style>
