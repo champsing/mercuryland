c9cf559c12bd6fb579ac2dceace89546e77a7518
champsing <88385811+champsing@users.noreply.github.com>
2024-11-04 02:21:11 +0800

    惡靈說要改繁體

--- web/App.vue
+++ web/App.vue
@@ -17,7 +17,7 @@ const tabs = [
     { path: "/publication", label: "資料公開" },
     { path: "/vod", label: "直播隨選" },
     { path: "/penalty", label: "直播懲罰" },
-    { path: "/wheel", label: "幸运转盘" },
+    { path: "/wheel", label: "幸運轉盤" },
     { path: "/contact", label: "聯絡我們" },
 ];
 </script>
