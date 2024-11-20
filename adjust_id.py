f2a6ca0a821e4cfd5e2c7c5dfff87305ed60a059
champsing <88385811+champsing@users.noreply.github.com>
2024-11-20 18:36:27 +0800

    git ignoring id resequencing tools

--- adjust_id.py
+++ adjust_id.py
@@ -4,7 +4,7 @@ with open('./id_wrong.json', 'r', encoding = 'UTF-8') as wrong:
     TBE = json.load(wrong)
 
 for i in range(len(TBE)) :
-    TBE[i]['id'] = 14 + i
+    TBE[i]['id'] = 97 + i
 
 with open('./id_right.json', 'w', encoding = 'UTF-8') as right:
     json.dump(TBE, right, ensure_ascii=False)
