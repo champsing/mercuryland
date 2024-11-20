502e8fe9f9c8c48bc17a43d8fd28c3bf45b951e4
champsing <88385811+champsing@users.noreply.github.com>
2024-11-20 18:53:55 +0800

    move original py to root directory

--- adjust_id.py
+++ adjust_id.py
@@ -4,7 +4,7 @@ with open('./id_wrong.json', 'r', encoding = 'UTF-8') as wrong:
     TBE = json.load(wrong)
 
 for i in range(len(TBE)) :
-    TBE[i]['id'] = 97 + i
+    TBE[i]['id'] = 97 + i # needed number + i
 
 with open('./id_right.json', 'w', encoding = 'UTF-8') as right:
     json.dump(TBE, right, ensure_ascii=False)
