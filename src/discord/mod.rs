21eb2eb9007ae870d7f5b99007f3e81d3c20518d
Logic <38597904+LogicFan@users.noreply.github.com>
2024-12-29 22:03:57 +0800

    add send_message method for discord bot

--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -1,12 +1,31 @@
 mod coin;
 mod wheel;
 
+use std::sync::OnceLock;
+
 use crate::{config::CONFIG, error::ServerError};
 use poise;
 use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
+use serenity::all::{ChannelId, CreateAttachment, Http};
+use std::sync::Arc;
 
 type Data = ();
 type Context<'a> = poise::Context<'a, Data, ServerError>;
+
+static HTTP: OnceLock<Arc<Http>> = OnceLock::new();
+
+// untested
+pub async fn send_message(
+    channel_id: ChannelId,
+    files: Vec<CreateAttachment>,
+    map: &impl serde::Serialize,
+) -> Result<(), ServerError> {
+    if let Some(http) = HTTP.get() {
+        http.send_message(channel_id, files, map).await?;
+    }
+
+    Ok(())
+}
 
 pub async fn run() -> Result<(), ServerError> {
     let options = poise::FrameworkOptions {
@@ -30,6 +49,8 @@ pub async fn run() -> Result<(), ServerError> {
     )
     .framework(framework)
     .await?;
+
+    HTTP.get_or_init(|| client.http.clone());
     client.start().await?;
 
     Ok(())
