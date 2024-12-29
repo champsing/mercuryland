abb956a5a7cfabaab898cb274e490a3dd27075fa
Logic <38597904+LogicFan@users.noreply.github.com>
2024-12-29 19:13:41 +0800

    add coin command

---
+++ src/discord/coin.rs
@@ -0,0 +1,23 @@
+use poise;
+use crate::{
+    database::{self, user::User as CoinUser},
+    error::ServerError,
+};
+
+#[poise::command(slash_command)]
+pub async fn coin(
+    ctx: super::Context<'_>,
+    #[description = "The channel id of youtube account"] channel: String,
+) -> Result<(), ServerError> {
+    let coin = {
+        let mut connection = database::get_connection()?;
+        let transaction = connection.transaction()?;
+        match CoinUser::by_id(channel, &transaction)? {
+            None => 0,
+            Some(u) => u.coin,
+        }
+    };
+
+    ctx.say(format!("{}", coin)).await?;
+    Ok(())
+}
