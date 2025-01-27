a0c96e80c739536a30c6dcee14fdbdecf912b367
champsing <88385811+champsing@users.noreply.github.com>
2025-01-27 23:50:31 +0800

    try export value

--- src/webpage/leaderboard/ldb.rs
+++ src/webpage/leaderboard/ldb.rs
@@ -1,8 +1,9 @@
 use crate::{
-    coin::youtube::Coin, database::{self, wheel::Wheel}, error::ServerError
+    coin::youtube::Coin,
+    database::{self},
+    error::ServerError,
 };
-use actix_web::{dev::Server, get, HttpResponse, Responder};
-use chrono::{TimeDelta, Utc};
+use actix_web::{get, HttpResponse, Responder};
 use serde::Serialize;
 
 #[derive(Debug, Clone, Serialize)]
@@ -14,15 +15,14 @@ struct Response {
 
 #[get("/api/leaderboard")]
 pub async fn handler() -> Result<impl Responder, ServerError> {
-    // let mut connection = database::get_connection()?;
-    // let transaction = connection.transaction()?;
-    // let export = Coin::export_all();
-    // println!("{:?}", Coin::export_all());
-    
-    // Ok(HttpResponse::Ok().json(Response {
-    //     id: export.id,
-    //     coin: export.coin,
-    //     display_name: export.display
-    // }))
-    Ok(HttpResponse::Ok())
+    let mut connection = database::get_connection()?;
+    let _transaction = connection.transaction()?;
+    let export = Coin::export_all()?;
+    println!("{:?}", Coin::export_all());
+
+    Ok(HttpResponse::Ok().json(Response {
+        id: serde_json::from_value(export[0].clone())?,
+        coin: serde_json::from_value(export[0].clone())?,
+        display_name: serde_json::from_value(export[0].clone())?,
+    }))
 }
