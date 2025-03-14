5ca7ac9e4a3d81a26cfec47f3797e610f7d21b74
champsing <88385811+champsing@users.noreply.github.com>
2025-03-15 07:39:45 +0800

    deprecate NGrid and NGi in Slide4.vue (img sizes need to fix)

--- web/components/welcome/Slide4.vue
+++ web/components/welcome/Slide4.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { NGrid, NGi } from "naive-ui";
 import { VaDivider } from "vuestic-ui";
 import NextPageButton from "./NextPageButton.vue";
 </script>
@@ -7,41 +6,32 @@ import NextPageButton from "./NextPageButton.vue";
 <template>
     <div class="h-screen overflow-hidden bg-black relative">
         <NextPageButton :page="4" />
-        <n-grid
-            x-gap="0"
-            :cols="2"
-            item-responsive
-            class="absolute x-center w-10/12 h-full"
+        <div
+            class="grid grid-cols-3 grid-rows-4 gap-4 absolute x-center w-5/6 h-full"
         >
-            <n-gi span="2 768:1" class="relative">
-                <div class="flex flex-col w-55% absolute x-center y-center">
-                    <div class="text-green-400 font-bold text-6xl">
-                        來這裡遇到同好。
-                    </div>
-                    <VaDivider class="mt-6 mb-6" />
-                    <div class="font-bold text-green-600 text-3xl">
-                        擁有豐富的擴充遊玩內容、玩家自辦活動、以及關服舉行的煙火大會。
-                    </div>
+            <div class="relative row-start-2 mt-16">
+                <div class="text-green-400 font-bold text-6xl">
+                    來這裡遇到同好。
                 </div>
-            </n-gi>
-            <n-gi span="0 768:1" class="relative">
-                <div class="flex flex-col absolute y-center x-center h-5/6 w-full">
-                    <div class="h-1/2 relative">
-                        <img
-                            src="@assets/images/welcome/s2_firework_1.png"
-                            class="absolute x-center y-center h-5/6 object-contain"
-                            alt="Welcome"
-                        />
-                    </div>
-                    <div class="h-1/2 relative">
-                        <img
-                            src="@assets/images/welcome/s2_firework_2.png"
-                            class="absolute x-center y-center h-5/6 object-contain"
-                            alt="Welcome"
-                        />
-                    </div>
+                <VaDivider class="mt-6 mb-6" />
+                <div class="font-bold text-green-600 text-3xl">
+                    擁有豐富的擴充遊玩內容、玩家自辦活動、以及關服舉行的煙火大會。
                 </div>
-            </n-gi>
-        </n-grid>
+            </div>
+            <div class="relative col-start-3 row-start-2">
+                <img
+                    src="@assets/images/welcome/s2_firework_1.png"
+                    class="absolute"
+                    alt="Welcome"
+                />
+            </div>
+            <div class="relative col-start-3 row-start-3 mt-4">
+                <img
+                    src="@assets/images/welcome/s2_firework_2.png"
+                    class="absolute"
+                    alt="Welcome"
+                />
+            </div>
+        </div>
     </div>
 </template>
