2574c68b1c9db8c416a4d0f690e19e63fce6b2c0
champsing <88385811+champsing@users.noreply.github.com>
2025-03-15 06:56:27 +0800

    deprecate NGrid and NGi in Slide3.vue

--- web/components/welcome/Slide3.vue
+++ web/components/welcome/Slide3.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { NGrid, NGi } from "naive-ui";
 import { VaDivider } from "vuestic-ui";
 import NextPageButton from "./NextPageButton.vue";
 </script>
@@ -7,21 +6,19 @@ import NextPageButton from "./NextPageButton.vue";
 <template>
     <div class="h-screen overflow-hidden bg-teal-800 relative">
         <NextPageButton :page="3" />
-        <n-grid
-            x-gap="0"
-            :cols="2"
-            item-responsive
-            class="absolute x-center w-10/12 h-full"
+        <div
+            class="grid grid-cols-3 grid-rows-3 gap-4 absolute x-center w-10/12 h-full"
         >
-            <n-gi span="0 768:1" class="relative">
+            <div class="relative row-start-1 row-end-4">
                 <img
                     src="@assets/images/welcome/statue_of_liberty.png"
                     class="absolute y-center x-center h-5/6 object-contain"
                     alt="Welcome"
                 />
-            </n-gi>
-            <n-gi span="2 768:1" class="relative">
-                <div class="flex flex-col w-3/5 absolute x-center y-center">
+            </div>
+            <div class="relative"></div>
+            <div class="relative col-start-3 row-start-2 -mt-4">
+                <div class="row-start-2 row-end-2">
                     <div class="text-yellow-400 font-bold text-6xl">
                         發揮您的創意，建造您夢想中的建築。
                     </div>
@@ -30,7 +27,7 @@ import NextPageButton from "./NextPageButton.vue";
                         絕無思想審查制度。您可以在我們的伺服器復刻世界上的有名建築，或單單只是在這個世界蓋出專屬於您的根據地。
                     </div>
                 </div>
-            </n-gi>
-        </n-grid>
+            </div>
+        </div>
     </div>
 </template>
