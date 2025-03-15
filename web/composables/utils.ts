e9fa3501d9cae31887448ef9c75ca557e13f5543
champsing <88385811+champsing@users.noreply.github.com>
2025-03-15 16:25:12 +0800

    make backToTop() exported

--- web/composables/utils.ts
+++ web/composables/utils.ts
@@ -72,6 +72,10 @@ export function remainingY(el) {
     );
 }
 
+export function backToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
 // export function interleave<T>(arr: T[], x: T): T[] {
 //     return arr.flatMap((e) => [e, x]).slice(0, -1);
 // }
