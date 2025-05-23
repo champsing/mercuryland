154ab4d8f08e80296782e8967fda77c850e039c6
champsing <88385811+champsing@users.noreply.github.com>
2025-05-24 01:57:46 +0800

    complete send jsonified ldb to front

--- src/webpage/leaderboard/ldb.rs
+++ src/webpage/leaderboard/ldb.rs
@@ -4,25 +4,13 @@ use crate::{
     error::ServerError,
 };
 use actix_web::{get, HttpResponse, Responder};
-use serde::Serialize;
-
-#[derive(Debug, Clone, Serialize)]
-struct Response {
-    id: u16,
-    coin: i64,
-    display_name: String,
-}
 
 #[get("/api/leaderboard")]
 pub async fn handler() -> Result<impl Responder, ServerError> {
     let mut connection = database::get_connection()?;
-    let _transaction = connection.transaction()?;
-    let export = Coin::export_all()?;
-    println!("{:?}", Coin::export_all());
+    let transaction = connection.transaction()?;
+    let export = Coin::all(&transaction)?;
+    println!("{:?}", export);
 
-    Ok(HttpResponse::Ok().json(Response {
-        id: serde_json::from_value(export[0].clone())?,
-        coin: serde_json::from_value(export[0].clone())?,
-        display_name: serde_json::from_value(export[0].clone())?,
-    }))
+    Ok(HttpResponse::Ok().json(export))
 }
