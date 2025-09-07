cf1d558fd864b6a445cd98d0c60a73f6a5c36171
LogicFan <fanyongda2012@gmail.com>
2025-09-07 09:24:45 -0400

    remove broken code

--- src/webpage/auth/google.rs
+++ src/webpage/auth/google.rs
@@ -1,12 +1,12 @@
-use actix_web::{post, web, HttpResponse, Responder};
-use chrono::DateTime;
-use jwt::SignWithKey;
-use serde::Deserialize;
-use std::{
-    fs::OpenOptions,
-    io::Write,
-    time::{Duration, SystemTime, UNIX_EPOCH},
-};
+// use actix_web::{post, web, HttpResponse, Responder};
+// use chrono::DateTime;
+// use jwt::SignWithKey;
+// use serde::Deserialize;
+// use std::{
+//     fs::OpenOptions,
+//     io::Write,
+//     time::{Duration, SystemTime, UNIX_EPOCH},
+// };
 
-#[post("/api/auth/google")]
-pub async fn handler() -> 
+// #[post("/api/auth/google")]
+// pub async fn handler() -> 
