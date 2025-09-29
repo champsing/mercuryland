393db08efaaf53d7fede7926358a7c5b2dfc4e8c
Logic <38597904+LogicFan@users.noreply.github.com>
2025-09-29 02:40:20 -0400

    Fix rust compiler warning.  (#71)
    * Fix warning
    
    * fixup

--- src/main.rs
+++ src/main.rs
@@ -7,45 +7,53 @@ fn main() -> Result<(), ServerError> {
 
     println!("======== server starting! ========");
 
-    let webpage = thread::spawn(|| loop {
-        let res = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .map_err(|err| ServerError::from(err))
-            .and_then(|rt| rt.block_on(async { webpage::run().await }));
+    let mut handles = vec![];
 
-        if let Some(err) = res.err() {
-            log::error!("restarting, webpage failed: {:?}", err)
+    handles.push(thread::spawn(|| {
+        loop {
+            let res = tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .map_err(|err| ServerError::from(err))
+                .and_then(|rt| rt.block_on(async { webpage::run().await }));
+
+            if let Some(err) = res.err() {
+                log::error!("restarting, webpage failed: {:?}", err);
+            }
         }
-    });
+    }));
 
-    let discord = thread::spawn(|| loop {
-        let res = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .map_err(|err| ServerError::from(err))
-            .and_then(|rt| rt.block_on(async { discord::run().await }));
+    handles.push(thread::spawn(|| {
+        loop {
+            let res = tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .map_err(|err| ServerError::from(err))
+                .and_then(|rt| rt.block_on(async { discord::run().await }));
 
-        if let Some(err) = res.err() {
-            log::error!("restarting, discord bot failed: {:?}", err)
+            if let Some(err) = res.err() {
+                log::error!("restarting, discord bot failed: {:?}", err);
+            }
         }
-    });
+    }));
 
-    let youtube = thread::spawn(|| loop {
-        let res = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .map_err(|err| ServerError::from(err))
-            .and_then(|rt| rt.block_on(async { youtube::run().await }));
+    handles.push(thread::spawn(|| {
+        loop {
+            let res = tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .map_err(|err| ServerError::from(err))
+                .and_then(|rt| rt.block_on(async { youtube::run().await }));
 
-        if let Some(err) = res.err() {
-            log::error!("restarting, youtube bot failed: {:?}", err)
+            if let Some(err) = res.err() {
+                log::error!("restarting, youtube bot failed: {:?}", err);
+            }
         }
-    });
+    }));
 
-    webpage.join().unwrap();
-    discord.join().unwrap();
-    youtube.join().unwrap();
+    for handle in handles {
+        handle.join().unwrap();
+    }
 
     Ok(())
 }
