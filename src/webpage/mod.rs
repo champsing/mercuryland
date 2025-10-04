2477d0d164831c7a69dc9a93bf64e9c30b3990bf
LogicFan <fanyongda2012@gmail.com>
2025-10-04 05:53:23 -0400

    add endpoint

--- src/webpage/mod.rs
+++ src/webpage/mod.rs
@@ -1,4 +1,5 @@
 pub mod auth;
+pub mod image;
 pub mod leaderboard;
 pub mod ping;
 pub mod video;
@@ -32,6 +33,8 @@ pub async fn run() -> Result<(), ServerError> {
             .service(video::update::handler)
             .service(video::metadata::handler)
             .service(leaderboard::get::handler)
+            .service(image::upload::handler)
+            .service(image::get::handler)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
