5a8ec1dddcec61967717aee538fa76bc443fd5f7
LogicFan <fanyongda2012@gmail.com>
2025-10-04 20:17:24 -0400

    add ViewPortHeight

---
+++ web/components/ViewportHeight.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { UseElementBounding } from "@vueuse/components";
+import { useWindowSize } from "@vueuse/core";
+
+export const FOOTNOTE_HEIGHT = 48;
+
+const vh = useWindowSize().height;
+function height(top: number) {
+    let height = vh.value - window.scrollY - top - FOOTNOTE_HEIGHT;
+    return height;
+}
+</script>
+
+<template>
+    <UseElementBounding v-slot="{ top }">
+        <div :style="{ height: `${height(top)}px` }">
+            <slot></slot>
+        </div>
+    </UseElementBounding>
+</template>
