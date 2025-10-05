d254224ad01447a86f88e5713c87301800323a80
LogicFan <fanyongda2012@gmail.com>
2025-10-05 04:16:04 -0400

    fixup

--- src/webpage/penalty/list.rs
+++ src/webpage/penalty/list.rs
@@ -3,31 +3,7 @@ use crate::{
     error::ServerError,
 };
 use actix_web::{HttpResponse, Responder, get};
-use chrono::NaiveDate;
-use serde::Serialize;
 
-#[derive(Serialize)]
-pub struct PenaltyResponse {
-    pub id: i64,
-    pub date: NaiveDate,
-    pub name: String,
-    pub detail: String,
-    pub state: i32,
-    pub history: Vec<(i32, NaiveDate)>,
-}
-
-impl From<Penalty> for PenaltyResponse {
-    fn from(penalty: Penalty) -> Self {
-        Self {
-            id: penalty.id,
-            date: penalty.date,
-            name: penalty.name,
-            detail: penalty.detail,
-            state: penalty.state.into(),
-            history: penalty.history.into_iter().map(|(state, date)| (state.into(), date)).collect(),
-        }
-    }
-}
 
 #[get("/api/penalty/list")]
 pub async fn handler() -> Result<impl Responder, ServerError> {
@@ -36,7 +12,7 @@ pub async fn handler() -> Result<impl Responder, ServerError> {
     let penalties = Penalty::all(&transaction)?;
     transaction.commit()?;
 
-    let responses: Vec<PenaltyResponse> = penalties.into_iter().map(PenaltyResponse::from).collect();
+    let responses: Vec<Penalty> = penalties;
 
     Ok(HttpResponse::Ok().json(responses))
 }
