7307cfd49beb6253cb8109bd32662e2b1d2d0f35
LogicFan <fanyongda2012@gmail.com>
2025-10-05 09:20:42 -0400

    add timeline

--- web/components/penalty/Detail.vue
+++ web/components/penalty/Detail.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
-import {
-    VaChip,
-    VaModal,
-} from "vuestic-ui";
+import { ref, watch } from "vue";
+import { VaChip, VaModal } from "vuestic-ui";
 import { BASE_URL } from "@/composables/utils";
 import { stateString, stateColor, PenItem } from "@/composables/penalty";
 import axios from "axios";
@@ -55,20 +52,61 @@ watch(
         close-button
     >
         <template v-if="penalty">
-        <div class="flex gap-4 items-center">
-            <VaChip
-                readonly
-                outline
-                size="small"
-                :color="stateColor(penalty.state, 'raw')"
-                class="w-24"
+            <div class="flex gap-4 items-center">
+                <VaChip
+                    readonly
+                    outline
+                    size="small"
+                    :color="stateColor(penalty.state, 'raw')"
+                    class="w-24"
+                >
+                    ● {{ stateString(penalty.state) }}
+                </VaChip>
+                <div class="truncate text-xl flex-1">{{ penalty.name }}</div>
+            </div>
+            <div
+                v-if="penalty.history && penalty.history.length > 0"
+                class="mt-4"
             >
-                ● {{ stateString(penalty.state) }}
-            </VaChip>
-            <div class="truncate text-xl flex-1">{{ penalty.name }}</div>
-        </div>
-        <div></div>
-        <div v-html="penalty.detail" class="mt-4"></div>
+                <div class="relative">
+                    <div
+                        class="absolute top-4 left-0 right-0 h-0.5 bg-gray-300"
+                    ></div>
+                    <div class="flex justify-between">
+                        <div
+                            v-for="(item, index) in penalty.history"
+                            :key="index"
+                            class="flex flex-col items-center"
+                            :style="{
+                                flex:
+                                    index === 0 ||
+                                    index === penalty.history.length - 1
+                                        ? '0 0 auto'
+                                        : '1',
+                            }"
+                        >
+                            <div
+                                class="w-3 h-3 rounded-full shadow-md"
+                                :style="{
+                                    backgroundColor: stateColor(item[0], 'raw'),
+                                }"
+                            ></div>
+                            <div class="text-xs text-center mt-1">
+                                <div
+                                    class="font-medium mt-1"
+                                    :style="{
+                                        color: stateColor(item[0], 'raw'),
+                                    }"
+                                >
+                                    {{ stateString(item[0]) }}
+                                </div>
+                                <div class="text-gray-500">{{ item[1] }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div v-html="penalty.detail" class="mt-4"></div>
         </template>
     </VaModal>
 </template>
