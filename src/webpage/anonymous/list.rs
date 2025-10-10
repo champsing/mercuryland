3c62d2cd65666fddcaeb3ccde7df58ebf049a4b4
LogicFan <fanyongda2012@gmail.com>
2025-10-10 11:26:12 -0400

    fixup

--- src/webpage/anonymous/list.rs
+++ src/webpage/anonymous/list.rs
@@ -5,9 +5,16 @@ use crate::{
     webpage::auth,
 };
 use actix_web::{HttpResponse, Responder, get, web};
+use chrono::{DateTime, Duration, Utc};
+use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use serenity::http::Http;
 use serenity::model::id::UserId;
+use std::collections::HashSet;
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+
+static USER_CACHE: Lazy<Arc<Mutex<HashMap<u64, (String, DateTime<Utc>)>>>> = Lazy::new(|| Arc::new(Mutex::new(HashMap::new())));
 
 #[derive(Serialize)]
 struct AnonymousResponse {
@@ -31,16 +38,30 @@ pub async fn handler(query: web::Query<Query>) -> Result<impl Responder, ServerE
     let anonymous_entries = Anonymous::all(&transaction)?;
     transaction.commit()?;
 
+    // Get unique author IDs
+    let unique_ids: HashSet<u64> = anonymous_entries.iter().map(|e| e.author as u64).collect();
+
     // Create HTTP client for Discord API
     let http = Http::new(&CFG_DISCORD_TOKEN);
 
-    // Convert Discord IDs to usernames
+    // Populate cache for missing users
+    {
+        let mut cache = USER_CACHE.lock().unwrap();
+        for &id in &unique_ids {
+            if !cache.contains_key(&id) || (Utc::now() - cache[&id].1) > Duration::hours(12) {
+                let username = match http.get_user(UserId::from(id)).await {
+                    Ok(user) => format!("{} ({})", user.name, id),
+                    Err(_) => format!("Unknown User ({})", id),
+                };
+                cache.insert(id, (username, Utc::now()));
+            }
+        }
+    }
+
+    // Build responses using cache
     let mut responses = Vec::new();
     for entry in anonymous_entries {
-        let username = match http.get_user(UserId::from(entry.author as u64)).await {
-            Ok(user) => format!("{} ({})", user.name, entry.author),
-            Err(_) => format!("Unknown User ({})", entry.author),
-        };
+        let (username, _) = USER_CACHE.lock().unwrap().get(&(entry.author as u64)).cloned().unwrap();
         responses.push(AnonymousResponse {
             id: entry.id,
             author: username,
