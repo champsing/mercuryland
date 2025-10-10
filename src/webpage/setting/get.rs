979473bc2707c1a09c104cb627e49c87a449df07
LogicFan <fanyongda2012@gmail.com>
2025-10-10 05:33:23 -0400

    fixup

--- src/webpage/setting/get.rs
+++ src/webpage/setting/get.rs
@@ -8,7 +8,7 @@ use serde_json;
 #[derive(Debug, Deserialize)]
 pub struct Query {
     pub token: String,
-    pub id: Option<i32>,
+    pub id: i32,
 }
 
 #[get("/api/setting/config")]
@@ -17,27 +17,21 @@ pub async fn handler(query: web::Query<Query>) -> Result<impl Responder, ServerE
         return Ok(HttpResponse::Forbidden().finish());
     }
 
+    if query.id == 0 { // WheelPassword is not retrievable
+        return Ok(HttpResponse::Ok().json(serde_json::json!({ "value": "" })))
+    }
+
     let mut connection = crate::database::get_connection()?;
     let transaction = connection.transaction()?;
 
-    if let Some(id) = query.id {
-        let config = match id {
-            1 => Config::ChannelPenalty,
-            2 => Config::ChannelCoin,
-            3 => Config::ChannelVote,
-            4 => Config::ChannelAnonymous,
-            _ => return Ok(HttpResponse::BadRequest().finish()),
-        };
-        let value = config.get(&transaction)?;
-        Ok(HttpResponse::Ok().json(serde_json::json!({ "id": id, "value": value })))
-    } else {
-        let configs = vec![
-            (0, Config::WheelPassword.get(&transaction)?),
-            (1, Config::ChannelPenalty.get(&transaction)?),
-            (2, Config::ChannelCoin.get(&transaction)?),
-            (3, Config::ChannelVote.get(&transaction)?),
-            (4, Config::ChannelAnonymous.get(&transaction)?),
-        ];
-        Ok(HttpResponse::Ok().json(configs))
-    }
+    let config = match query.id {
+        1 => Config::ChannelPenalty,
+        2 => Config::ChannelCoin,
+        3 => Config::ChannelVote,
+        4 => Config::ChannelAnonymous,
+        _ => return Ok(HttpResponse::BadRequest().finish()),
+    };
+    let value = config.get(&transaction)?;
+    Ok(HttpResponse::Ok().json(serde_json::json!({ "value": value })))
+
 }
