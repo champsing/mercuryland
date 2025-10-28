82d7bdb791c687c275b779a883ec8a73d6b3c73b
champsing <champsingxiang@gmail.com>
2025-10-29 04:31:34 +0800

    respond VN to front end

--- src/webpage/ping.rs
+++ src/webpage/ping.rs
@@ -3,5 +3,5 @@ use actix_web::{HttpResponse, Responder, get};
 
 #[get("/api/ping")]
 pub async fn handler() -> Result<impl Responder, ServerError> {
-    Ok(HttpResponse::Ok().json(serde_json::json!({"status": "operational"})))
+    Ok(HttpResponse::Ok().json(serde_json::json!({"status": "operational", "version": env!("CARGO_PKG_VERSION")})))
 }
