3d90caa5f0bea3180138b824fe89b3b5008bce70
champsing <champsingxiang@gmail.com>
2025-10-29 04:32:57 +0800

    format and consist VN

--- src/webpage/ping.rs
+++ src/webpage/ping.rs
@@ -3,5 +3,6 @@ use actix_web::{HttpResponse, Responder, get};
 
 #[get("/api/ping")]
 pub async fn handler() -> Result<impl Responder, ServerError> {
-    Ok(HttpResponse::Ok().json(serde_json::json!({"status": "operational", "version": env!("CARGO_PKG_VERSION")})))
+    Ok(HttpResponse::Ok()
+        .json(serde_json::json!({"status": "operational", "version": env!("CARGO_PKG_VERSION")})))
 }
