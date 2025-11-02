3aab968cb549da8e8eb2e0866a8247fade0829b6
香榭 <88385811+champsing@users.noreply.github.com>
2025-11-03 07:08:10 +0800

    Fix/wheelsubmit (#157)
    * make auth sate a condition to decide whether submit available or not
    
    * change auth method to verify token
    
    * 2.18.4
    
    * cargo patch 2.18.4
    
    * rm run_number

--- web/components/wheel/Wheel.vue
+++ web/components/wheel/Wheel.vue
@@ -19,6 +19,7 @@ import {
     ArrowClockwise24Filled,
     ArrowSyncCheckmark24Filled,
 } from "@vicons/fluent";
+import { useAuthState } from "@/composables/authState";
 
 document.title = "幸運轉盤 - 水星人的夢幻樂園";
 
@@ -26,6 +27,8 @@ interface WheelItem {
     label: string;
     weight: number;
 }
+
+const authState = useAuthState();
 
 const re = /x[1-9][0-9]*$/;
 const wheelRef = ref<any>(null);
@@ -91,9 +94,15 @@ const textArea2 = defineModel("textArea2", {
 });
 
 function submit(hide?: CallableFunction) {
+    const token = localStorage.getItem("token");
+    if (!token) {
+        window.alert("請先登入管理員帳號");
+        return;
+    }
+
     axios
         .post(BASE_URL + "/api/wheel/submit", {
-            password: modal3.password,
+            token: token,
             penalties: textArea.value.split("\n").filter((x) => x != ""),
         })
         .then((response) => {
@@ -108,7 +117,7 @@ function submit(hide?: CallableFunction) {
             console.log(error);
             useToast().init({
                 duration: 2000,
-                message: "廣播失敗",
+                message: "廣播失敗，請重新登入或再試一次",
                 color: "danger",
             });
             modal3.password = "";
@@ -197,6 +206,13 @@ const modal3 = reactive({
     password: "",
     fail: false,
 });
+
+const isSubmitAvailable = ref<boolean | null>(
+    isSpinning.value ||
+        count(textArea2.value) == 0 ||
+        !APIstatus.value ||
+        !authState.isAuthenticated,
+);
 </script>
 
 <template>
@@ -224,13 +240,11 @@ const modal3 = reactive({
                 <VaButton
                     class="w-full mt-8"
                     @click="modal3.show = true"
-                    :disabled="
-                        isSpinning || count(textArea2) == 0 || !APIstatus
-                    "
+                    :disabled="isSubmitAvailable"
                 >
                     完成抽選
                 </VaButton>
-                <div class="text-red-500 text-right" v-if="!APIstatus">
+                <div class="text-red-500 text-right" v-if="!isSubmitAvailable">
                     <VaDivider color="danger" orientation="right">
                         停用
                     </VaDivider>
